5f8700d57d1e7472226b7b93afa78dbc40250492
Hyungoo Lee <hyungoo.dylan@gmail.com>
2017-02-24 21:52:40 +1200

    Add Repository

---
+++ RosteringSystem/Models/Repository.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace RosteringSystem.Models
+{
+    public class Repository
+    {
+        public IEnumerable<Shift> GetShiftList()
+        {
+            using (var context = new RosterContext())
+            {
+                return context.Shifts.ToList();
+            }
+        }
+
+        public IEnumerable<Staff> GetStaffList()
+        {
+            using (var context = new RosterContext())
+            {
+                return context.Staff.ToList();
+            }
+        }
+    }
+}
