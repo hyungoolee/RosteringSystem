7c7c0b5aab5c46a0da0d30a0bf38c43954f54801
Romelyn Ungab <romelyn.ungab@gmail.com>
2017-02-28 09:42:05 +1300

    Added ViewModels folder

---
+++ RosteringSystem/ViewModels/AddShiftView.cs
@@ -0,0 +1,13 @@
+﻿using RosteringSystem.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RosteringSystem.ViewModels
+{
+    public class AddShiftView
+    {
+        Job jobsList 
+    }
+}
---
+++ RosteringSystem/ViewModels/ShiftClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RosteringSystem.ViewModels
+{
+    public class ShiftClass
+    {
+
+    }
+}
